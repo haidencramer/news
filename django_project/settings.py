7e0e6ced691a22a9b734d5f8405ee8dbe09cbca4
Haiden Cramer <cramerhaiden@yahoo.com>
2024-10-28 13:04:47 -0600

    newspaper app

--- django_project/settings.py
+++ django_project/settings.py
@@ -41,7 +41,10 @@ INSTALLED_APPS = [
     "crispy_bootstrap5",
     "accounts",
     "pages",
+    "articles",
 ]
+
+
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
@@ -109,7 +112,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = "en-us"
 
-TIME_ZONE = "UTC"
+TIME_ZONE = "MST"
 
 USE_I18N = True
 
